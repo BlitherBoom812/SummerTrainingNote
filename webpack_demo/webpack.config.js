6e91a2dc4d71c44993e6195527a5de994904550a
BlitherBoom812 <tsz21@mails.tsinghua.edu.cn>
2022-07-15 13:34:45 +0800

    add webpack demo

---
+++ webpack_demo/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader'],
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
